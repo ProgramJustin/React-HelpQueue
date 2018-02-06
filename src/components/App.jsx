6b9c1cb9384ee5201c65b7920f352a2bfc9fa1f9
Justin Gomez <programjustin@gmail.com>
2018-02-06 12:05:52 -0800

    create Header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 // Components are also modular. They aren't aware of their dependency to parent components. They only "know" about themselves, any data (props) passed to them, and child components they're responsible for rendering.
 import React from "react";
+import Header from "./Header";
 
 function App(){
   return (
     <div>
-      <h1>Help Queue</h1>
+      <Header/>,
       <h3>3a</h3>
       <h3>Justin, Mew, and Nigel</h3>
       <p><em>Firebase entries not saving!</em></p>
