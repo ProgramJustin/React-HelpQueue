78cb76255f73958e4858589d31df11d3d4fad872
Justin Gomez <programjustin@gmail.com>
2018-02-06 14:10:06 -0800

    access props in receiving component

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-function Ticket(){
+// Notice we've also added props as an argument to the Ticket component function's method signature (function Ticket(props)) to indicate it should now accept props. Again, React is functional programming. So the props we pass down are technically just an argument to the component's function!
+function Ticket(props){
+// Also, don't forget JSX JavaScript expressions must be wrapped in curly braces. This indicates content should be evaluated instead of literally rendered. We call the props object and access its location, name and issue values, which requires evaluation. (Without curly braces, our DOM would literally read "props.location" instead of "3A").
   return (
     <div>
-       <h3>3A - Justin, Mew, and Nigel</h3>
-       <p><em>Firebase entries not saving!</em></p>
+    <h3>{props.location} - {props.names}</h3>
+    <p><em>{props.issue}</em></p>
        <hr/>
     </div>
   );
