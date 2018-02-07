58fc1433fc3c23816f6e050764e1caa3308e48be
Justin Gomez <programjustin@gmail.com>
2018-02-06 18:31:48 -0800

    add eslint

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React from 'react'
 // React also allows us to assign strict data types to props with a special propTypes property.
- // allows us to strictly define what data type props must be.
-import PropTypes from "prop-types";
+// allows us to strictly define what data type props must be.
+import PropTypes from 'prop-types'
 
 // Notice we've also added props as an argument to the Ticket component function's method signature (function Ticket(props)) to indicate it should now accept props. Again, React is functional programming. So the props we pass down are technically just an argument to the component's function!
 function Ticket(props){
 // Also, don't forget JSX JavaScript expressions must be wrapped in curly braces. This indicates content should be evaluated instead of literally rendered. We call the props object and access its location, name and issue values, which requires evaluation. (Without curly braces, our DOM would literally read "props.location" instead of "3A").
   return (
     <div>
-    <h3>{props.location} - {props.names}</h3>
-    <p><em>{props.issue}</em></p>
-       <hr/>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
     </div>
-  );
+  )
 }
 // Notice the first letter in the propTypes following Ticket is lowercase (Ticket.propTypes), whereas the PropTypes in the lines defining each component property is uppercase (PropTypes.string).
 // We fill this object literal with a list of props the component accepts, and the data type each should be:
@@ -20,6 +20,6 @@ Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   issue: PropTypes.string
-};
+}
 
-export default Ticket;
+export default Ticket
--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,24 @@
 // This entry point is a special type of file. It is not a component
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import { AppContainer } from 'react-hot-loader';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import { AppContainer } from 'react-hot-loader'
 // <AppContainer> is a wrapper component from React-Hot-Loader that handles reloading the application and sending errors if anything goes awry.
 
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-    <Component/>
+      <Component/>
     </AppContainer>,
     document.getElementById('react-app-root')
-  );
-};
-render(App);
+  )
+}
+render(App)
 // The if statement below render() triggers the swapping process. Take a look at the HMR documentation for optional further explanation about how this works behind the scenes.
 if (module.hot) {
   module.hot.accept('./components/App', () => {
     render(App)
-  });
+  })
 }
 
 // We always import React from the "react" npm package at the top of the file.
