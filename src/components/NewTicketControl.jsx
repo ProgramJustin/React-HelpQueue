9c9b2ef173815b6f3809b72eb12fb241d903b3e5
Justin Gomez <programjustin@gmail.com>
2018-02-09 13:39:30 -0800

    add state

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class NewTicketControl extends React.Component {
+  constructor(props) {
+    // super is called to access a parent class's constructor
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
   render(){
     return(
       <div>
