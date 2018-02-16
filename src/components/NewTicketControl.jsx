2fdf8f29aeef8582430810dc24e62dc42e96c726
Justin Gomez <programjustin@gmail.com>
2018-02-16 00:03:53 -0800

    update PropTypes

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ConfirmationQuestions from './ConfirmationQuestions';
 import NewTicketForm from './NewTicketForm';
+import PropTypes from 'prop-types';
 
 class NewTicketControl extends React.Component {
   constructor(props) {
@@ -30,4 +31,9 @@ class NewTicketControl extends React.Component {
     );
   }
 }
+
+NewTicketControl.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
+
 export default NewTicketControl;
