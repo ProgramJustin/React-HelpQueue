ee62ee9eb01136000106a5bcb950bd44576c9b96
Justin Gomez <programjustin@gmail.com>
2018-02-16 11:16:53 -0800

    add unique id to each ticket in ticket list using uuid

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -3,17 +3,18 @@ import Ticket from './Ticket';
 import PropTypes from 'prop-types';
 
 function TicketList(props){
+  console.log(props.ticketList);
   var ticketList = {
     fontFamily: 'sans-serif',
   };
   return (
     <div style={ticketList}>
       <hr/>
-      {props.ticketList.map((ticket, index) =>
+      {props.ticketList.map((ticket) =>
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
-          key={index}
+          key={ticket.id}
         />
       )}
     </div>
