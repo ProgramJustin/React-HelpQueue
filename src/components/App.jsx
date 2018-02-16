d5b9a37e418cd6b3912e615963e0d650f3a2202b
Justin Gomez <programjustin@gmail.com>
2018-02-16 00:43:57 -0800

    fix bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,9 +14,10 @@ class App extends React.Component {
     this.state = {
       masterTicketList: []
     };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
   }
   handleAddingNewTicketToList(newTicket){
-    var newMasterTicketList = this.masterTicketList.slice();
+    var newMasterTicketList = this.state.masterTicketList.slice();
     newMasterTicketList.push(newTicket);
     this.setState({masterTicketList: newMasterTicketList});
   }
