16ef565448973bb5adcc462a6ae08559aaa9f3fb
Justin Gomez <programjustin@gmail.com>
2018-02-16 12:01:59 -0800

    implement Moment to time stamp each ticket in ticketList

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 // React also allows us to assign strict data types to props with a special propTypes property.
 // allows us to strictly define what data type props must be.
 import PropTypes from 'prop-types';
+import Moment from 'moment';
 
 // Notice we've also added props as an argument to the Ticket component function's method signature (function Ticket(props)) to indicate it should now accept props. Again, React is functional programming. So the props we pass down are technically just an argument to the component's function!
 function Ticket(props){
@@ -17,17 +18,24 @@ function Ticket(props){
         }
       `}</style>
       <h3>{props.location} - {props.names}</h3>
+      <h4>{displayTimeOpen(props.timeOpen)} ago</h4>
       <p><em>{props.issue}</em></p>
       <hr/>
     </div>
   );
 }
+
+function displayTimeOpen(timeOpen) {
+  return timeOpen.from(new Moment(), true);
+}
+
 // Notice the first letter in the propTypes following Ticket is lowercase (Ticket.propTypes), whereas the PropTypes in the lines defining each component property is uppercase (PropTypes.string).
 // We fill this object literal with a list of props the component accepts, and the data type each should be:
 Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  issue: PropTypes.string
+  issue: PropTypes.string,
+  timeOpen: PropTypes.instanceOf(Moment).isRequired
 };
 
 export default Ticket;
--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Ticket from './Ticket';
 import PropTypes from 'prop-types';
+import Moment from 'moment';
 
 function TicketList(props){
   console.log(props.ticketList);
@@ -14,6 +15,7 @@ function TicketList(props){
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
+          timeOpen={ticket.timeOpen}
           key={ticket.id}
         />
       )}
