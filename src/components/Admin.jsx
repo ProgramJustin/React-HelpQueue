807dcd7b90521cabdb22c415aa47e69f53df0dd8
Justin Gomez <programjustin@gmail.com>
2018-02-23 11:22:30 -0800

    update the Admin component to display the ticket information we've just passed into it

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TicketList from './TicketList';
 
-function Admin() {
+function Admin(props) {
   return (
     <div>
       <h2>Admin</h2>
+      <TicketList ticketList={props.ticketList} />
     </div>
   );
 }
