d1a1be4bf980719f5421f6e7189176d0bb9218f4
Justin Gomez <programjustin@gmail.com>
2018-02-23 12:15:45 -0800

    update prop types

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -31,7 +31,8 @@ Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   issue: PropTypes.string,
-  formattedWaitTime: PropTypes.string.isRequired
+  formattedWaitTime: PropTypes.string.isRequired,
+  currentRouterPath: PropTypes.string
 };
 
 export default Ticket;
