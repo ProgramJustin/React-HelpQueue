45ab5d56924fe45cab24cc8ef84f19ac50eee073
Justin Gomez <programjustin@gmail.com>
2018-02-26 00:57:20 -0800

    declare new prop type

--- src/components/TicketDetail.jsx
+++ src/components/TicketDetail.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function TicketDetail() {
 
   return (
     <p>Test</p>
   );
+}
+
+TicketDetail.propTypes = {
+  selectedTicket: PropTypes.object
 }
 
 export default TicketDetail;
