75053d81501233bba30433f7bd44568edc049c6b
Justin Gomez <programjustin@gmail.com>
2018-03-15 13:39:59 -0700

    declare action in reducer, add example ticket information  for testing purposes

--- _tests_/ticket-list-reducer.test.js
+++ _tests_/ticket-list-reducer.test.js
@@ -2,6 +2,15 @@ import ticketListReducer from './../src/reducers/ticket-list-reducer';
 
 describe('ticketListReducer', () => {
 
+  let action;
+  const sampleTicketData = {
+    names : 'Justin & Nigel',
+    location : '4b',
+    issue : 'Jest is triflen and will not work with webpack',
+    timeOpen: 150000000000,
+    id: 0
+  };
+
   test('Should return default state if no action type is recognized', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
