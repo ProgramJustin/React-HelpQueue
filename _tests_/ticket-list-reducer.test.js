a0bcc72374e62d7f5e12ac7e1c982a235150a70d
Justin Gomez <programjustin@gmail.com>
2018-03-14 19:10:37 -0700

    import reducer into test file

--- _tests_/ticket-list-reducer.test.js
+++ _tests_/ticket-list-reducer.test.js
@@ -1,3 +1,5 @@
+import ticketListReducer from './../src/reducers/ticket-list-reducer';
+
 describe('ticketListReducer', () => {
   test("Temp test: 2 + 2 = 4", () => {
     expect(2 + 2).toEqual(4);
