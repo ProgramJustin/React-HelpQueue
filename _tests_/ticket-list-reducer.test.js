4d4ca4a6285bc7a4e976e24801294f9f1eddf9c8
Justin Gomez <programjustin@gmail.com>
2018-03-15 13:49:14 -0700

    add a test uses sample data to confirm our reducer can add new tickets to Redux store

--- _tests_/ticket-list-reducer.test.js
+++ _tests_/ticket-list-reducer.test.js
@@ -15,4 +15,26 @@ describe('ticketListReducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
 
+  test('Should successfully add new ticket data to masterTicketList', () => {
+    const { names, location, issue, timeOpen, id } = sampleTicketData;
+    action = {
+      type: 'ADD_TICKET',
+      name: names,
+      location: location,
+      issue, issue,
+      timeOpen: timeOpen,
+      id: id
+    };
+
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id
+      }
+    });
+  });
+
 });
