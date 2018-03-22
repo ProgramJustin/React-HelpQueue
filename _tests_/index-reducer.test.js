e23502fd5699d8024ec013da190a59803142802b
Justin Gomez <programjustin@gmail.com>
2018-03-22 10:17:44 -0700

    confirm rootReducer returns correct logic from both sub-reducers

--- _tests_/index-reducer.test.js
+++ _tests_/index-reducer.test.js
@@ -3,10 +3,15 @@ import rootReducer from './../src/reducers/index';
 describe('rootReducer', () => {
 
   test('Should return default state if no action type is recognized', () => {
-    expect(rootReducer({}, { type: null })).toEqual({
-      masterTicketList: {},
-      selectedTicket: {}
-    });
+    expect(rootReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should contain ticketListReducer logic', () => {
+    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, { type: null }));
+  });
+
+  test('Should contain selectedTicketReducer logic', () => {
+    expect(store.getState().selectedTicket).toEqual(selectedTicketReducer(undefined, { type: null }));
   });
 
 });
