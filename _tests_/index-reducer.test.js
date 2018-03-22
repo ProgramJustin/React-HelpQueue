c240391e1d15b29ebaed12a8be015df06b78b5e2
Justin Gomez <programjustin@gmail.com>
2018-03-22 11:00:17 -0700

    import rootReducer into index.jsx and provide it to createStore()

--- _tests_/index-reducer.test.js
+++ _tests_/index-reducer.test.js
@@ -1,3 +1,4 @@
+// This reducers/index.js file is something called a module index
 import rootReducer from './../src/reducers/index';
 import selectedTicketReducer from './../src/reducers/selected-ticket-reducer';
 import ticketListReducer from './../src/reducers/ticket-list-reducer';
