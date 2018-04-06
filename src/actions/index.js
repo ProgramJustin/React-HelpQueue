07113b4ca2265d2dcfeaf1fb17f09085dfdd67b8
Justin Gomez <programjustin@gmail.com>
2018-04-06 10:45:15 -0700

    push() a ticket object literal to the tickets ref

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,15 @@ const { firebaseConfig } = constants;
 import firebase from 'firebase';
 
 firebase.initializeApp(firebaseConfig);
+// specify a tickets location (also known as a node) in our database
+// You can reference the root or child location in your Database by calling firebase.database().ref() or firebase.database().ref("child/path").
+const tickets = firebase.database().ref('tickets');
+
+export function addTicket(_names, _location, _issue) {
+  return () => tickets.push({
+    names: _names,
+    location: _location,
+    issue: _issue,
+    timeOpen: new Date().getTime()
+  });
+}
