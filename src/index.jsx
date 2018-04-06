73605f3efbb2fccb6484425b694b369475c2fb07
Justin Gomez <programjustin@gmail.com>
2018-04-06 11:48:30 -0700

    install, import and apply Redux-Thunk middleware to manage async actions

--- src/index.jsx
+++ src/index.jsx
@@ -5,11 +5,12 @@ import App from './components/App';
 // import { AppContainer } from 'react-hot-loader';
 // <AppContainer> is a wrapper component from React-Hot-Loader that handles reloading the application and sending errors if anything goes away.
 import { HashRouter } from 'react-router-dom';
-import { createStore} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 /*eslint-disable */
 
 let unsubscribe = store.subscribe(() =>
